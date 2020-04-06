20b497f645350fa14ed5ad22fc4db8b4133de173
Frank Joke <frankjoke@hotmail.com>
2020-04-07 01:51:58 +0200

    Removed Electron

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,9 @@ process.env.VUE_APP_VERSION = require("./package.json").version;
 module.exports = {
   transpileDependencies: ["vuetify", "vue-echarts", "resize-detector"],
 
-  pluginOptions: {
-    electronBuilder: {
-      builderOptions: {
-        win: {
-          icon: "public/favicon.ico",
-        },
-        extraResources: [
-          {
-            from: "./extraResources/",
-            to: "extraResources",
-            filter: ["**/*"],
-          },
-        ],
-      },
-    },
-  },
+  pluginOptions: {},
 
   productionSourceMap: false,
-  publicPath: '.',
-  outputDir: 'coronafj'
+  publicPath: ".",
+  outputDir: "docs",
 };
